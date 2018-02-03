11ead709ed852158694ee71b6cdd90e5aae0dc75
Adam Mescher <admescher@gmail.com>
2018-02-03 14:28:51 -0700

    Update fetchHelpers by removing unnecessary spaces

--- src/utils/fetchHelpers.js
+++ src/utils/fetchHelpers.js
@@ -1,34 +1,33 @@
 import {
   userDataObject,
   portfolioQuotes,
-  combinedStockCurrentValue } from '../actions/PortfolioContainerActions';
+  combinedStockCurrentValue } from '../Portfolio/actions';
 import { iexURL } from './iexURL';
-import { firebaseURL } from './firebaseURL';
-import { firebaseDatabaseSecret } from './firebaseDatabaseSecret';
-import { stockDataObjectToDisplay } from '../actions/SearchResultsActions';
+import firebaseDatabaseSecret from './firebaseDatabaseSecret';
+import { stockDataObjectToDisplay } from '../SearchResults/actions';
 
 export const fetchStockQuote = symbol => dispatch => {
   if (!symbol) { return; }
 
-  return fetch( `${iexURL}/stock/${symbol}/quote` )
-    .then( res => res.json() )
-    .then( res => cleanStockQuoteData( res ) )
-    .then( res => dispatch( stockDataObjectToDisplay( res ) ) );
+  return fetch(`${iexURL}/stock/${symbol}/quote`)
+    .then(res => res.json())
+    .then(res => cleanStockQuoteData(res))
+    .then(res => dispatch(stockDataObjectToDisplay(res)));
 };
 
 export const fetchPortfolioQuotesHelper = portfolio => dispatch => {
-  if ( !portfolio ) { return; }
+  if (!portfolio) { return; }
 
-  const stockQuotes = Object.keys( portfolio ).map( symbol => {
+  const stockQuotes = Object.keys(portfolio).map(symbol => {
     return fetch(`${iexURL}/stock/${symbol}/quote`)
-      .then( res => res.json() );
+      .then(res => res.json());
   });
 
-  Promise.all( stockQuotes )
-    .then( res => {
-      dispatch( portfolioQuotes( res ) );
+  Promise.all(stockQuotes)
+    .then(res => {
+      dispatch(portfolioQuotes(res));
       dispatch(
-        combinedStockCurrentValue( calculateStockValue( res, portfolio ) )
+        combinedStockCurrentValue(calculateStockValue(res, portfolio))
       );
     });
 };
@@ -52,17 +51,17 @@ const cleanStockQuoteData = ({
 });
 
 const calculateStockValue =
-(quotes, portfolio) => quotes.reduce( (acc, quote) => {
+(quotes, portfolio) => quotes.reduce((acc, quote) => {
   return acc += quote.latestPrice * portfolio[quote.symbol].numberOfShares;
 }, 0);
 
 export const fetchUserDataHelper = userID => dispatch => {
-  if ( !userID ) { return; }
-
+  const firebaseURL = 'https://possibull-5f513.firebaseio.com';
+  if (!userID) { return; }
   return fetch(
     `${firebaseURL}/users/${userID}.json?auth=${firebaseDatabaseSecret}`
   )
-    .then(res => res.json() )
-    .then(res => dispatch( userDataObject( res ) ) )
-    .catch(error => alert( error ) );
+    .then(res => res.json())
+    .then(res => dispatch(userDataObject(res)))
+    .catch(error => alert(error));
 };
