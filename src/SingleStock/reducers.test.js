9d51737357c8a9f721329deeac54105bba1d89b7
Adam Mescher <admescher@gmail.com>
2018-02-03 14:20:01 -0700

    Add SingleStock directory and relevant files

---
+++ src/SingleStock/reducers.test.js
@@ -0,0 +1,23 @@
+import * as reducers from './reducers';
+
+describe('SINGLE STOCK REDUCER', () => {
+  it('should pass a stock purchase data object action', () => {
+    const mockState = {
+      GOOGL: {
+        symbol: 'GOOGL',
+        numberOfShares: 50
+      }
+    };
+    const mockStockPurchase = {
+      symbol: 'F',
+      numberOfShares: 50
+    };
+    const action = {
+      type: 'ADD_STOCK_PURCHASE_TO_USER_PORTFOLIO',
+      userPortfolio: [mockStockPurchase]
+    };
+    const expectation = action.userPortfolio;
+
+    expect(true).toBe(true);
+  });
+});
